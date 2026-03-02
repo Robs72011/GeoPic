e47bf2776766b85969fd125cf24ded63decf60f2
GiovanniSpoleto <giovanniscuola2002@gmail.com>
2026-03-02 14:07:10 +0100

    controller link rappresenta

--- GeoPic/src/main/java/Controller/Controller.java
+++ GeoPic/src/main/java/Controller/Controller.java
@@ -345,12 +345,27 @@ public class Controller {
             if (foto != null && soggetto != null){
                 foto.addSoggetto(soggetto);
                 soggetto.addFotoInCuiAppare(foto);
-
-
             }
-
         }
+    }
 
+    public void linkUtenteSoggetto(){
+        ArrayList<String> tmpIdUtente =  new ArrayList<>();
+        ArrayList<String> tmpSoggetto = new ArrayList<>();
+
+        soggettoPostgresDAO.getRappresenta(tmpSoggetto, tmpIdUtente);
+
+
+        for (int i=0;i<tmpIdUtente.size();i++) {
+            Soggetto soggetto = getSoggettoByNomeSoggetto(soggettiInMemory, tmpSoggetto.get(i));
+            Utente utente = getUtenteByID(utentiInMemory, tmpIdUtente.get(i));
+
+
+            if (utente != null && soggetto != null) {
+                utente.setSoggetto(true);
+                soggetto.setUtenteRappresentato(utente);
+            }
+        }
     }
 
     public void linkOwnerGalleries(){
