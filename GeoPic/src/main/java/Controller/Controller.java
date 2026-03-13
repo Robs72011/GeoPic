e67ab753b790589c3b35598f045750c4c0d26222
Gabrytrac <gabry00trac@gmail.com>
2026-03-13 20:07:00 +0100

    Rimozione utente usa la nuova funzione eliminazioneUtenteByAdmin

--- GeoPic/src/main/java/Controller/Controller.java
+++ GeoPic/src/main/java/Controller/Controller.java
@@ -824,15 +824,6 @@ public class Controller {
     }
 
     /**
-     * Rimuove un utente dal sistema (con conseguenze a cascata su foto, gallerie, ecc.).
-     */
-    public void eliminaUtente(int idUtente) {
-        utentePostgresDAO.deleteUtente(idUtente);
-        // Poichè potrebbe scatenare eliminazioni a cascata nel DB su altre entità, ricaricamo tutto per sicurezza
-        loadInMemory();
-    }
-
-    /**
      * Ritorna la galleria privata dell'utente che ha eseguito il log in.
      * @return La galleria privata dell'utente loggato
      */
